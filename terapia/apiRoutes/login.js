f0013d11d66899a68cc78b3107460bf0d1f8d1ce
Felipe Beskow <beskow@pop-os.localdomain>
2020-12-05 04:00:03 -0300

    Tela de Login - finalização

--- terapia/apiRoutes/login.js
+++ terapia/apiRoutes/login.js
@@ -1,12 +1,21 @@
 let NeDB = require('nedb');
-let db = new NeDB({
+let dbLogin = new NeDB({
     filename:'db/login.db',
     autoload:true,
 });
+let dbLogLogin = new NeDB({
+    filename:'db/login-log.db',
+    autoload:true
+});
 
 module.exports = app => {
-    app.put('/l',(req,res)=>{
-        db.findOne({
+    app.get('/l',(req,res)=>{
+        res.status(200).json({
+            mensagem: 'Oi, to funfando'
+        });
+    });
+    app.post('/l',(req,res)=>{
+        dbLogin.findOne({
             _login:req.body['_login']
         }).exec((e,user)=>{
             if (e){
@@ -15,10 +24,26 @@ module.exports = app => {
                 res.status(400).json({
                     error: e
                 });  
-            } else if(user['_password']==req.body['_password']){
+            } else if( ( user != undefined ) && ( user['_password'] == req.body['_password'] ) ){
                 console.log("Login efetuado:",req.body['_login']);
-                res.status(200).json({_id:user['_id']});
+                dbLogLogin.insert({
+                    _idLogin:user['_login'],
+                    _datetime: new Date().getTime()
+                },(er,log)=>{
+                    if(e) {
+                        console.error(er);
+                        res.status(400).json({
+                            error: er
+                        });
+                    } else {
+                        console.log(log);
+                        res.status(200).json({
+                            _id:log['_id']
+                        });
+                    }
+                });
             } else {
+                console.log(req.body);
                 console.log("Login negado:",req.body['_login']); 
                 res.status(400).json({
                     error: 'login not found'
@@ -26,19 +51,20 @@ module.exports = app => {
             }
         });
     });
-    app.put('/l/id',(req,res)=>{
-        db.findOne({
+    app.put('/lr',(req,res)=>{
+        dbLogLogin.findOne({
             _id:req.body['_id']
-        }).exec((e,user)=>{
+        }).exec((e,login)=>{
+            
             if (e){
                 console.log("Id negado:",req.body['_id']);
                 console.error(e);
                 res.status(400).json({
                     error: e
                 });  
-            } else if(user){
-                console.log("Login com id efetuado:",req.body['_id']);
-                res.status(200).json(user);
+            } else if( ( login != undefined ) && ((new Date().getTime() - parseInt(login['_datetime']) < 259200000) ) ){
+                console.log("Login renovado:",req.body['_id']);
+                res.status(200).json(login);
             } else {
                 console.log("Id negado:",req.body['_id']); 
                 res.status(400).json({
@@ -49,7 +75,7 @@ module.exports = app => {
     });
     app.post('/l/:passadmin',(req,res)=>{
         if (req.params.passadmin == '***REMOVED***') {
-            db.insert(req.body,(e,user)=>{
+            dbLogin.insert(req.body,(e,user)=>{
                 if (e){
                     console.log("Erro ao registar:",req.body);
                     console.error(e);
