902ecd7ae9b50e16f1972d2eabd3b074047bdda9
Felipe Beskow <felipebeskow@outlook.com>
2020-12-11 15:07:41 -0300

    autenticação requisição /c #3

--- terapia/apiRoutes/clients.js
+++ terapia/apiRoutes/clients.js
@@ -1,24 +1,59 @@
 let NeDB = require('nedb');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 let db = new NeDB({
     filename:'db/clients.db',
     autoload:true,
 });
 
+
+
 module.exports = app =>{
 
-    app.get('/c',(req,res)=>{
-        
-        db.find({}).sort({name:1}).exec((err,clients)=>{
-            if (err){
-                console.error(err);
-                res.status(400).json({
-                    error: err
-                });
-            }else{
-                res.status(200).json({clients});
-            }
-        });
+    app.put('/c',(req,res)=>{
 
+        let ajax = new XMLHttpRequest();
+        let message = '';
+        let passou = false;
+        
+        ajax.open('PUT', 'http://localhost:3000/lr');
+
+        message = `_id=${req.body['_id']}`;
+    
+
+        ajax.onload = e => {
+            try{
+                ajax.responseText.parse
+                if( (JSON.parse(ajax.responseText)!=undefined) && (JSON.parse(ajax.responseText)['error']==undefined) ){
+                    passou = true;
+                    db.find({}).sort({name:1}).exec((err,clients)=>{
+                        if (err){
+                            console.error(err);
+                            res.status(400).json({
+                                'error': err
+                            });
+                        }else{
+                            res.status(200).json({clients});
+                        }
+                    });
+                } else {
+                    console.log('Login negado');
+                    res.status(400).json({
+                        'error': 'Login Negado'
+                    });                    
+                }
+            } catch(error) {
+                console.error(error);
+                res.status(400).json({
+                    'error': error,
+                    'ajax': ajax.responseText,
+                    'passou': passou
+                });
+            }
+        }
+
+        ajax.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+        ajax.send(message);
     });
 
     app.post('/c',(req,res)=>{
@@ -78,5 +113,7 @@ module.exports = app =>{
             }
         });
     });
+
+
 
 };
