63fe0d4e178c864f72f99c21865267452ad32a9e
Felipe Beskow <felipebeskow@outlook.com>
2023-12-08 00:51:53 -0300

    Criação de tabelas

--- scr/init_sql.js
+++ scr/init_sql.js
@@ -3,8 +3,7 @@ import { sql } from '../db.js';
 
 const InitSQL = async () => {
     const response = await sql`select version()`;
-    console.log(response)
-    
+    console.log(response)  
 };
 
 export default InitSQL;
