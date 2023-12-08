037a79083658b34018429a2e424c0ff133994ccc
Felipe Helfensteler Beskow <felipebeskow@users.noreply.github.com>
2023-12-08 13:45:13 -0300

    Insert de password padrão

--- terapia.sql
+++ terapia.sql
@@ -23,4 +23,10 @@ select
 	'admin' login, 
 	'User default' display_name
 where not exists (select * from aut_login);
+insert into aut_password(login_id, password, actual_flag)
+select 
+	(select id from aut_login where login = 'admin') login_id, 
+	'7407946a7a90b037ba5e825040f184a142161e4c61d81feb83ec8c7f011a99b0d77f39c9170c3231e1003c5cf859c69bd93043b095feff5cce6f6d45ec513764' password,  --cryptoJs.SHA3("admin").toString()
+	true actual_flag
+where not exists (select * from aut_password);
 commit;
